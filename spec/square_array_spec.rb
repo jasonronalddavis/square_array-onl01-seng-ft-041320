55c5ad257630fd7f25e3fca0015d3e283e15bfc3
Jason Davis <jdavis@c.ringling.edu>
2020-04-23 17:25:08 +0000

    Automatically backed up by Learn

--- spec/square_array_spec.rb
+++ spec/square_array_spec.rb
@@ -5,8 +5,8 @@ describe '#square_array' do
     expect(numbers).to_not receive(:collect)
     expect(numbers).to_not receive(:map)
     expect(numbers).to_not receive(:inject)
-    #square_array(numbers)
-    square_array([1,2,3])
+    square_array(numbers)
+    #square_array([1,2,3])
   end
 
   it 'calls on each' do
